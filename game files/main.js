087f3b4a17899dde1007a3a32aa4bdb16cadd5ec
Marientje1 <94905244+Marientje1@users.noreply.github.com>
2025-12-17 11:43:13 +0000

    Dec 17, 2025, 11:43 AM

--- game files/main.js
+++ game files/main.js
@@ -25,10 +25,20 @@ var tileset;
 var layer;
 
 function preload() {
-    // Laad assets (voeg je eigen afbeeldingen toe aan assets/ map)
-    this.load.image('tiles', 'assets/tiles.png'); // Voorbeeld: spritesheet met tegels
-    this.load.spritesheet('player', 'assets/player.png', { frameWidth: 32, frameHeight: 32 }); // Speler sprite
-    // Als je geen assets hebt, gebruik Phaser's ingebouwde graphics voor prototyping
+    // Creëer eenvoudige textures voor tegels en speler (geen externe assets nodig)
+    // Tegels: groen voor gras, bruin voor aarde
+    let tileGraphics = this.add.graphics();
+    tileGraphics.fillStyle(0x00ff00); // Gras
+    tileGraphics.fillRect(0, 0, 32, 32);
+    tileGraphics.fillStyle(0x8B4513); // Aarde
+    tileGraphics.fillRect(32, 0, 32, 32);
+    tileGraphics.generateTexture('tiles', 64, 32);
+
+    // Speler: blauw vierkant
+    let playerGraphics = this.add.graphics();
+    playerGraphics.fillStyle(0x0000ff);
+    playerGraphics.fillRect(0, 0, 32, 32);
+    playerGraphics.generateTexture('player', 32, 32);
 }
 
 function create() {
@@ -40,7 +50,7 @@ function create() {
     // Stel botsingen in voor bepaalde tegels (bijv. aarde)
     layer.setCollision([1, 2]); // Pas aan op basis van je tileset
 
-    // Maak speler
+    // Maak speler (eenvoudig zonder animaties)
     player = this.physics.add.sprite(100, 100, 'player');
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
@@ -52,25 +62,6 @@ function create() {
 
     // Controls
     cursors = this.input.keyboard.createCursorKeys();
-
-    // Animaties voor speler (optioneel)
-    this.anims.create({
-        key: 'left',
-        frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
-        frameRate: 10,
-        repeat: -1
-    });
-    this.anims.create({
-        key: 'turn',
-        frames: [{ key: 'player', frame: 4 }],
-        frameRate: 20
-    });
-    this.anims.create({
-        key: 'right',
-        frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
-        frameRate: 10,
-        repeat: -1
-    });
 }
 
 function update() {
@@ -96,13 +87,10 @@ function createWorldData() {
     return [
         [0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [1,1,1,1,1,1,1,1,1,1],
-        [2,2,2,2,2,2,2,2,2,2],
-        [2,2,2,2,2,2,2,2,2,2],
+    } else if (cursors.right.isDown) {
+        player.setVelocityX(160);
+    } else {
+        player.setVelocityX(0
         [2,2,2,2,2,2,2,2,2,2]
     ];
 }
